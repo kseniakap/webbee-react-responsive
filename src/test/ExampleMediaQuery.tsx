c66e52f03ba60cceecbbe429c85607c14df51515
ksenia_kap_1 <kseniakap096@gmail.com>
2024-07-10 15:51:23 +0300

    fix

--- src/test/ExampleMediaQuery.tsx
+++ src/test/ExampleMediaQuery.tsx
@@ -9,9 +9,6 @@ export const ExampleMediaQuery = () => (
         <p>You also have a huge screen</p>
       </MediaQuery>
     </MediaQuery>
-    <MediaQuery orientation='portrait'>
-        <p>You also have a huge screen</p>
-      </MediaQuery>
     <MediaQuery minResolution="2dppx">
       {(matches) =>
         matches ? <p>You are retina</p> : <p>You are not retina</p>
